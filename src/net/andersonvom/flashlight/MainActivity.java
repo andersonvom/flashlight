40e7b262742e487651651fe3e1ec914d710f3963
Anderson Mesquita <andersonvom@gmail.com>
2013-02-21 10:34:39 -0300

    Added basic flashlight toggling interaction

--- src/net/andersonvom/flashlight/MainActivity.java
+++ src/net/andersonvom/flashlight/MainActivity.java
@@ -2,14 +2,24 @@ package net.andersonvom.flashlight;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener
+{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		TextView screen = (TextView) findViewById(R.id.toggle_button);
+		screen.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +27,35 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void onClick(View v)
+	{
+		Context context = v.getContext();
+		toggleFlashlight(context);
+	}
+
+	private void toggleFlashlight(Context context)
+	{
+		boolean hasFlash = context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+		if (hasFlash)
+		{
+			Toast.makeText(context, R.string.flashlight_toggle_msg, Toast.LENGTH_SHORT).show();
+			toggleCameraFlash(context);
+		}
+		else
+		{
+			Toast.makeText(context, R.string.flashlight_not_found, Toast.LENGTH_SHORT).show();
+			toggleScreen();
+		}
+	}
+
+	private void toggleCameraFlash(Context context)
+	{
+	}
+
+	private void toggleScreen()
+	{
 	}
 
 }
