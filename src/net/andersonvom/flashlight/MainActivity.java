682db288468362fe5afbb0583a7ad556423d4397
Anderson Mesquita <andersonvom@gmail.com>
2013-02-21 15:48:40 -0300

    Added basic flashlight operation (on/off)

--- src/net/andersonvom/flashlight/MainActivity.java
+++ src/net/andersonvom/flashlight/MainActivity.java
@@ -1,9 +1,12 @@
 package net.andersonvom.flashlight;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,6 +15,7 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener
 {
+	public static Camera cam;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +56,25 @@ public class MainActivity extends Activity implements OnClickListener
 
 	private void toggleCameraFlash(Context context)
 	{
+		if (cam == null) cam = Camera.open();
+
+		Parameters p = cam.getParameters();
+		if (p.getFlashMode().equals(Parameters.FLASH_MODE_OFF))
+		{
+			Log.w("[Flashlight]", "Flash TORCH....");
+			p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+			cam.setParameters(p);
+			cam.startPreview();
+		}
+		else
+		{
+			Log.w("[Flashlight]", "Flash OFF...");
+			p.setFlashMode(Parameters.FLASH_MODE_OFF);
+			cam.setParameters(p);
+			cam.stopPreview();
+			cam.release();
+			cam = null;
+		}
 	}
 
 	private void toggleScreen()
