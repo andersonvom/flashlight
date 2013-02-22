f255cd30b7c8b97c5633941d3078a9f6dcde1b8d
Anderson Mesquita <andersonvom@gmail.com>
2013-02-22 02:05:49 -0300

    Added basic settings activity

--- src/net/andersonvom/flashlight/MainActivity.java
+++ src/net/andersonvom/flashlight/MainActivity.java
@@ -2,6 +2,7 @@ package net.andersonvom.flashlight;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
@@ -9,6 +10,7 @@ import android.hardware.Camera.Parameters;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
@@ -40,6 +42,19 @@ public class MainActivity extends Activity implements OnClickListener
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case R.id.action_settings:
+				startActivity(new Intent(this, SettingsActivity.class));
+				break;
+		}
+
 		return true;
 	}
 
